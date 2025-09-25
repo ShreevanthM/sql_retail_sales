f793fd48f671e8a517ba5a88af929d71c71342c1
ShreevanthM <197445761+ShreevanthM@users.noreply.github.com>
2025-09-25 22:55:01 +0530

    Update sql_project_01.sql

--- sql_project_01.sql
+++ sql_project_01.sql
@@ -29,29 +29,29 @@ quantiy  is null or
 price_per_unit  is null or 
 cogs  is null or 
 total_sale  is null ;
-
+1
 select * from retail_sales where category='Clothing' and quantiy>=4 and to_char(sale_date,'yyyy-mm')='2022-11'
-
+2
  select * from retail_sales 
 where category='Clothing' and
 quantiy>=4 and 
 extract (year  from sale_date)='2022' and 
 extract (month from sale_date )='11'
 
- 
+3 
 select category ,sum(total_sale) as total from retail_sales
 group by category
-
+4
 select avg(age)as avg_age from retail_sales
 where category ='Beauty'
-
+5
 select * from retail_sales 
 where total_sale>1000
-
+6
 select category , gender , count(*) as  ti_ni from retail_sales 
 group by 1,2 
 order by 1
-
+7
 select * from (
 select extract(YEAR from sale_date) as year  ,
 extract(month from sale_date) as mount,
@@ -62,18 +62,17 @@ group by 1 ,2) as t1
 where rank=1
 --order by 1,3 desc
 
-
+8
 select customer_id,sum(total_sale) as total from retail_sales 
 group by 1
-
 order by total desc
 limit 5
-
+9
 select category , count(distinct customer_id) as counte 
 from retail_sales 
 group by 1
 
-
+10
 with  new_table as(
 select *, 
 case 
@@ -86,3 +85,4 @@ select shift, count (*) as no_order from new_table
 group by 1 
 
 order by 1
+
